924f091b84a5ec46964a8becdafd557b877be2df
saoodahmad <46574631+saoodahmad@users.noreply.github.com>
2023-01-04 01:56:04 +0530

    chore: add jest, husky and lint-staged

---
+++ src/__tests__/hello.test.ts
@@ -0,0 +1,7 @@
+describe('Sample test', () => {
+    it('should pass', () => {
+        const num1 = 2
+        const num2 = 3
+        expect(num1 + num2).toBe(5)
+    })
+})
---
+++ src/add.ts
@@ -0,0 +1,5 @@
+const add = (): number => {
+    return 3
+}
+
+export default add
