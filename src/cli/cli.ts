de2f6ddcff14663ad1618d077046b88dae900809
saoodahmad <46574631+saoodahmad@users.noreply.github.com>
2023-01-14 22:52:33 +0530

    patch: improved shell and support for reading external file

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
 import chalk from 'chalk'
+import * as fs from 'fs'
+import { createInterface } from 'readline'
 import run from '../index'
 
 console.info(
@@ -18,17 +20,22 @@ console.info(
 const args: string[] = process.argv.slice(2)
 
 if (args.length === 0) {
-    // prompt the user for input
     process.stdout.write('> ')
 
-    process.stdin.on('data', (data: Buffer) => {
-        const userInput = data.toString().trim()
-        run(userInput)
+    const readline = createInterface({
+        input: process.stdin,
+    })
+
+    readline.on('line', (line) => {
+        run(line)
     })
 } else if (args.length === 1) {
-    // read from file and evaluate
-
-    throw new Error('No implementation found')
+    try {
+        const sourceCode = fs.readFileSync(args[0], 'utf8')
+        run(sourceCode)
+    } catch (error) {
+        console.error(chalk.red(`File not found at path ${args[0]}`))
+    }
 } else {
     console.info(chalk.hex('#83aaff')(`\tcommand: npm run cli <filepath>`))
 }
